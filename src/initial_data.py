14a96fb6b3389a1a41b196459bd58240eae46ea8
hleyf <hleyf@hotmail.com>
2023-12-23 22:40:43 +0000

    Add Faker library and create_users function

--- src/initial_data.py
+++ src/initial_data.py
@@ -2,11 +2,10 @@
 from datetime import datetime
 from models import db
 from werkzeug.security import generate_password_hash
-
-
+from models import User
+from faker import Faker
 
 def create_admin():
-    from models import User
     admin = User.query.filter_by(email='admin@admin.com').first()
     if admin is None:
         user = User(
@@ -22,4 +21,24 @@ def create_admin():
         hashed_password = generate_password_hash(user.password, method='pbkdf2:sha256')
         user.password = hashed_password  # Use the User object passed in as a parameter
         db.session.add(user)
+        db.session.commit()
+
+def create_users():
+    previous_users = User.query.all()
+    if len(previous_users) <= 1:
+        fake = Faker()
+        # generate 30 random users and adds them to the database
+        for i in range(30):
+            user = User(
+                user_name=f'user{i}',
+                email=fake.email(),
+                password='123',
+                full_name=fake.name() + ' ' + fake.last_name(),
+                role='user',
+                created_at=datetime.now(),
+                created_by='system'
+            )
+            hashed_password = generate_password_hash(user.password, method='pbkdf2:sha256')
+            user.password = hashed_password
+            db.session.add(user)
         db.session.commit()
