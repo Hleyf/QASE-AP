458b446f65a5d6aad175c811361e136ab7d80e84
hleyf <hleyf@hotmail.com>
2023-12-29 18:00:55 +0000

    Update task and user routes

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, logout_user
 from services import UserService
+from models import User
 
 
 
@@ -28,20 +29,29 @@ def check_email_availability(email):
     response = service.get_email_availability(email)
     return response
 
-@user_routes.route('/users/<id>/edit', methods=['GET','POST'])
+@user_routes.route('/user/<id>/update', methods=['POST'])
 @login_required
 def update_user(id):
-    if request.method == 'GET':
-        is_admin = current_user.role == 'admin'
-        user = service.get_user_by_id(id)
-        is_user = current_user.id == user.id
-        return render_template('pages/edit-modal.html', is_admin=is_admin, is_user = is_user, user=user)
-    elif request.method == 'POST':
-        user_id = service.update_user(id)
-        if user_id:
-            return redirect(url_for('main.home'))
-        else:
-            return render_template('pages/edit-modal.html', error="Invalid credentials")
+    user_id = service.update_user(id)
+    if user_id:
+        return redirect(url_for('user_routes.users'))
+         
+@user_routes.route('/user/new', methods=['POST'])
+@login_required
+def create_new_user():
+    # Create User object from form data
+    user = User(
+        user_name=request.form['user_name'],
+        full_name=request.form['full_name'],
+        email=request.form['email'],
+        password=request.form['password'],
+        role=request.form['role'] if request.form['role'] else 'user'
+    )
+    user_id = service.create_user(user)
+    if user_id:
+        return redirect(url_for('user_routes.users'))
+    else:
+        return render_template('pages/edit-modal.html', error="Invalid credentials")
         
 @user_routes.route('/users/<id>/delete', methods=['DELETE'])
 @login_required
@@ -62,3 +72,13 @@ def current_user_role():
         return jsonify({'id': current_user.id, 'role': current_user.role})
     else:
         return "User not authenticated"
+    
+    
+@user_routes.route('/users/search', methods=['GET'])
+@login_required
+def search_users():
+    term = request.args.get('q')
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    users = service.search_users(term, page, per_page)
+    return jsonify({'items': [user.to_dict() for user in users]})
